27b6b58517cd5d08c7b74b53612b1757c6b147ef
github username <github email address>
2020-06-04 15:17:30 -0400

    more seed data, god, cat, item

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
     )
 end
 
-400.times do
+100.times do
     Pet.create(
         name: Faker::FunnyName.name,
         age: Faker::Number.between(from: 1, to: 20).to_i,
@@ -16,3 +16,24 @@ end
         care_instructions: Faker::Lorem.sentence,
         user_id: Faker::Number.between(from: 1, to: 100).to_i
     )
+end
+
+200.times do 
+    Pet.create(
+        name: Faker::Creature::Dog.name,
+        age: Faker::Number.between(from: 1, to: 20).to_i,
+        category: "Dog",
+        care_instructions: Faker::Lorem.sentence,
+        user_id: Faker::Number.between(from: 1, to: 100).to_i
+    )
+end
+
+200.times do
+    Pet.create(
+        name: Faker::Creature::Cat.name,
+        age: Faker::Number.between(from: 1, to: 20).to_i,
+        category: "cat",
+        care_instructions: Faker::Lorem.sentence,
+        user_id: Faker::Number.between(from: 1, to: 100).to_i
+    )
+end
