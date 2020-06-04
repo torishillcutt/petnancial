44a52577d340d9b6e88bc069658ee7dea0bbc2c7
github username <github email address>
2020-06-04 15:26:01 -0400

    more item seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,10 +38,36 @@ end
     )
 end
 
-1000.times do
+200.times do
     Item.create(
         cost: Faker::Number.between(from: 1, to: 100).to_i,
         order_date: Faker::Date.between(from: 30.days.ago, to: Date.today),
-        
+        arrival_date: Faker::Date.between(from: 30.days.ago, to: 20.days.ago),
+        category: "Food"
+        pet_id: Faker::Number.between(from: 1, to: 500).to_i
     )
+end
+
+200.times do
+    Item.create(
+        cost: Faker::Number.between(from: 1, to: 100).to_i,
+        order_date: Faker::Date.between(from: 365.days.ago, to: Date.today),
+        arrival_date: Faker::Date.between(from: 365.days.ago, to: 20.days.ago),
+        category: "toy"
+        pet_id: Faker::Number.between(from: 1, to: 500).to_i
+    )
+end
+
+200.times do
+    Item.create(
+        cost: Faker::Number.between(from: 1, to: 100).to_i,
+        order_date: Faker::Date.between(from: 365.days.ago, to: Date.today),
+        arrival_date: Faker::Date.between(from: 365.days.ago, to: 20.days.ago),
+        category: "accessory"
+        pet_id: Faker::Number.between(from: 1, to: 500).to_i
+    )
+end
+
+
+
 
