59b80e2eb79aff9ff1f0a704906b55b83646d54e
github username <github email address>
2020-06-04 14:53:26 -0400

    pet seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
 100.times do
+    name = Faker::Name.name
     User.create(
-        name: Faker::Name.name,
-        password: Faker::Alphanumeric.alphanumeric(number: 10, min_alpha: 3, min_numeric: 3),
-        email: Faker::Internet.safe_email,
+        name: name,
+        password: "password",
+        email: Faker::Internet.safe_email(name: "#{name}"),
         location: Faker::Address.city
+    )
+end
+
+400.times do
+    Pet.create(
+        name: Faker::FunnyName.name,
+        age: Faker::Number.between(from: 1, to: 20).to_i,
+        category: Faker::Creature::Animal.name,
+        care_instructions: Faker::Lorem.sentence,
+        user_id: Faker::Number.between(from: 1, to: 100).to_i
     )
