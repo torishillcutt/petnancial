46b993e21b1ee89eba48c624bd2ddc599d38e21e
github username <github email address>
2020-06-05 15:48:44 -0400

    conditionals in pet average cost update in item callback method)

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,37 +4,56 @@ class Item < ActiveRecord::Base
 
 
     ######CALL BACK MUST ACCOUNT FOR IF THERE IS NO VISITS, OR ONLY ONE VISIT
-  after_create do
+  after_save do
     pet = Pet.find(self.pet.id)
     if pet.visits.length > 1 && pet.items.length > 1
       first_visit = pet.visits.order(:date).first.date
       last_visit = pet.visits.order(:date).last.date
       visit_time_diff = Time.diff(first_visit, last_visit)
-      visit_average = pet.visits.sum(:cost) / visit_time_diff[:month]
+      if visit_time_diff[:month].to_i != 0
+        visit_average = pet.visits.sum(:cost) / visit_time_diff[:month].to_i
 
-      first_item = pet.items.order(:order_date).first.order_date
-      last_item = pet.items.order(:order_date).last.order_date
-      item_time_diff = Time.diff(first_item, last_item)
-      item_average = pet.items.sum(:cost) / item_time_diff[:month]
+        first_item = pet.items.order(:order_date).first.order_date
+        last_item = pet.items.order(:order_date).last.order_date
+        item_time_diff = Time.diff(first_item, last_item)
+        if item_time_diff[:month].to_i != 0
+          item_average = pet.items.sum(:cost) / item_time_diff[:month].to_i
 
-      pet.average_cost = item_average + visit_average
+          pet.average_cost = item_average.to_i + visit_average.to_i
+          pet.save
+        end
+      end
     elsif pet.visits.length == 0
-      first_item = pet.items.order(:order_date).first.order_date
-      last_item = pet.items.order(:order_date).last.order_date
-      item_time_diff = Time.diff(first_item, last_item)
-      pet.average_cost = pet.items.sum(:cost) / item_time_diff[:month]
+        if pet.items.length > 1
+            first_item = pet.items.order(:order_date).first.order_date
+            last_item = pet.items.order(:order_date).last.order_date
+            item_time_diff = Time.diff(first_item, last_item)
+            if item_time_diff[:month].to_i != 0
+              pet.average_cost = pet.items.sum(:cost).to_i / item_time_diff[:month].to_i
+              pet.save
+            end
+        elsif pet.items.length == 1
+            pet.average_cost = pet.items.first.cost.to_i
+            pet.save
+        end
     elsif pet.visits.length == 1 
         if pet.items.length == 1
            cost = pet.items.first.cost + pet.visits.first.cost
            time_diff = Time.diff(pet.items.order(:order_date).first.order_date, pet.visits.order(:date).first.date)
-           pet.average_cost = cost / time_diff[:month]
+           if time_diff[:month].to_i != 0
+             pet.average_cost = cost / time_diff[:month].to_i
+             pet.save
+           end
         else
           first_item = pet.items.order(:order_date).first.order_date
           last_item = pet.items.order(:order_date).last.order_date
           item_time_diff = Time.diff(first_item, last_item)
-          item_average = pet.items.sum(:cost) / item_time_diff[:month]
-          cost =  pet.visits.first.cost + pet.items.sum(:cost)
-          pet.average_cost = cost / item_time_diff[:month]
+          if item_time_diff[:month] != 0
+            item_average = pet.items.sum(:cost) / item_time_diff[:month].to_i
+            cost =  pet.visits.first.cost.to_i + pet.items.sum(:cost).to_i
+            pet.average_cost = cost / item_time_diff[:month].to_i
+            pet.save
+          end
         end
     end
   end
