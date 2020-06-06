edec01291f296cd93ef4f1afebf822e321d792a4
github username <github email address>
2020-06-06 19:42:05 -0400

    item delete route

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,7 +17,7 @@ class ItemsController < ApplicationController
     @item = Item.create(
       name: params[:name],
       category: params[:category],
-      cost: params[:cost],
+      cost: params[:cost].to_i,
       order_date: params[:order_date],
       pet_id: params[:id]
     )
@@ -41,7 +41,7 @@ class ItemsController < ApplicationController
     end
   end
 
-  # GET: /items/5/edit
+
   get "/users/pets/:id/items/:item_id/edit" do
     @pet = Pet.find(params[:id])
     @user = User.find(@pet.user_id)
@@ -56,11 +56,31 @@ class ItemsController < ApplicationController
 
   # PATCH: /items/5
   patch "/users/pets/:id/items/:item_id" do
-    redirect "/items/:id"
+    binding.pry
+    @item = Item.find(params[:item_id])
+    @item.name = params[:name]
+    @item.cost = params[:cost].to_i
+    @item.category = params[:category]
+    @item.order_date = params[:order_date]
+    if @item.save
+      redirect "/users/pets/#{@item.pet_id}/items/#{@item.id}"
+    else
+      @error = "please try again!"
+      erb :"/items/edit.html"
+    end
   end
 
   # DELETE: /items/5/delete
-  delete "/items/:id/delete" do
-    redirect "/items"
+  delete "/users/pets/:id/items/:item_id/delete" do
+    @pet = Pet.find(params[:id])
+    @user = User.find(@pet.user_id)
+    @item = Item.find(params[:item_id])
+    if logged_in? && @user == current_user
+      @item.delete
+      redirect "/users/pets/#{@pet.id}"
+    else
+      @error = "invalid credentials"
+      redirect '/users/login'
+    end
   end
 end
